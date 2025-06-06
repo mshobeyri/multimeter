a46c6af7ed19e0900edadb0adf2254068a25e933
Mehrdad Shobeyri <mehrdad.shobeyri@gmail.com>
2025-06-06 13:19:20 +0200

    Invalidate items that changed in combobox

--- mmtview/src/components/ComboTable.tsx
+++ mmtview/src/components/ComboTable.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 
 interface ComboTablePair {
   name: string;
@@ -13,22 +14,34 @@ interface ComboTableProps {
 const ComboTable: React.FC<ComboTableProps> = ({ pairs, onChange }) => (
   <table style={{ width: "100%", borderCollapse: "collapse" }}>
     <tbody>
-      {pairs.map(pair => (
-        <tr key={pair.name}>
-          <td style={{ padding: "8px" }}>{pair.name}</td>
-          <td style={{ padding: "8px" }}>
-            <select
-              value={pair.value}
-              onChange={e => onChange(pair.name, e.target.value)}
-              style={{ width: "100%" }}
-            >
-              {pair.options.map(opt => (
-                <option key={opt} value={opt}>{opt}</option>
-              ))}
-            </select>
-          </td>
-        </tr>
-      ))}
+      {pairs.map(pair => {
+        const isValid = pair.options.includes(pair.value);
+        return (
+          <tr key={pair.name}>
+            <td style={{ padding: "8px" }}>{pair.name}</td>
+            <td style={{ padding: "8px" }}>
+              <select
+                value={pair.value}
+                onChange={e => onChange(pair.name, e.target.value)}
+                style={{
+                  width: "100%",
+                  color: isValid ? undefined : "red",
+                  borderColor: isValid ? undefined : "red"
+                }}
+              >
+                {!isValid && pair.value && (
+                  <option value={pair.value} disabled style={{ color: "red" }}>
+                    {pair.value} (invalid)
+                  </option>
+                )}
+                {pair.options.map(opt => (
+                  <option key={opt} value={opt}>{opt}</option>
+                ))}
+              </select>
+            </td>
+          </tr>
+        );
+      })}
     </tbody>
   </table>
 );
