5f8bf214c8db2d75b622e8a1f438f08b581a1ef9
Mehrdad Shobeyri <mehrdad.shobeyri@gmail.com>
2025-07-01 08:05:20 +0200

    Add test data

---
+++ mmtview/src/components/TestData.tsx
@@ -0,0 +1,70 @@
+
+import {Type} from "./CommonData"
+import {Protocol, Format, Method} from "./APIData";
+
+export type Timestr = `${number}s` | `${number}m` | `${number}h` | "inf";
+export type Repeat = `${number}` | "inf";
+
+export type Parameter = { [key: string]: string };
+
+export interface TestImportItem {
+  [key: string]: string;
+}
+
+export interface TestMetric {
+  repeat?: Repeat;
+  threads?: number;
+  duration?: Timestr;
+  rampup?: Timestr;
+}
+
+export interface TestFlowCallTest {
+  call?: string;
+  interface?: string;
+  endpoint?: string;
+  headers?: Record<string, string>;
+  inputs?: Record<string, any>;
+  outputs?: Record<string, any>;
+  body?: string;
+}
+
+export interface TestFlowCallAPI {
+  call?: string;
+  interface?: string;
+  endpoint?: string;
+  headers?: Record<string, string>;
+  inputs?: Record<string, any>;
+  outputs?: Record<string, any>;
+  body?: string;
+}
+
+export interface TestFlowCallDirect {
+  call: null;
+  protocol: Protocol;
+  format: Format;
+  endpoint: string;
+  method?: Method;
+  headers?: Record<string, string>;
+  body?: string | object;
+  query?: Record<string, string>;
+  params?: Record<string, string>;
+  cookies?: Record<string, string>;
+}
+
+export interface TestFlowCheck {
+  check: string;
+}
+
+export type TestFlowStep = TestFlowCallTest | TestFlowCallDirect | TestFlowCallAPI | TestFlowCheck;
+
+export interface TestData {
+  type: Type;
+  title: string;
+  tags: string[];
+  description: string;
+  import?: Parameter[];
+  inputs?: Parameter[];
+  outputs?: Parameter[];
+  metrics?: TestMetric;
+  flow?: TestFlowStep[];
+}
