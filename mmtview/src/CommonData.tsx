ada48e92c0b891f85151e4d3a592417a3e645a6b
Mehrdad Shobeyri <mehrdad.shobeyri@gmail.com>
2025-07-15 00:04:59 +0200

    Disable test type

--- mmtview/src/CommonData.tsx
+++ mmtview/src/CommonData.tsx
@@ -8,12 +8,11 @@ export const jsonTypes = [
   "object", "object[]", "string", "string[]", "number", "number[]", "boolean", "boolean[]"
 ];
 
-
 export const typeOptions = [
   { value: "api", label: "API" },
   { value: "env", label: "Environment" },
   { value: "var", label: "Variables" },
-  { value: "test", label: "Test" }
+  // { value: "test", label: "Test" }
 ];
 
 export interface MMTFile {
