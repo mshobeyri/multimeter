a33fb4d3e46e102d8dab7bdf6f387f76981beac7
Mehrdad Shobeyri <mehrdad.shobeyri@gmail.com>
2025-07-27 09:21:01 +0200

    Add multimeter activity menu

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,40 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   context.subscriptions.push(showPreviewCommand);
+
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      'multimeterPanel',
+      new MultimeterPanelProvider(context)
+    )
+  );
 }
 
 export function deactivate() {}
+
+class MultimeterPanelProvider implements vscode.WebviewViewProvider {
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    webviewView.webview.options = {
+      enableScripts: true,
+    };
+    webviewView.webview.html = this.getHtml();
+  }
+
+  getHtml() {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+      <body>
+        <h2>Welcome to Multimeter!</h2>
+        <p>This is your custom side panel.</p>
+      </body>
+      </html>
+    `;
+  }
+}
