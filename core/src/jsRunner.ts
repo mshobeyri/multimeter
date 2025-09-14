b573fdd663ce3f39a60f07b1599056611f7a69fc
Mehrdad Shobeyri <mehrdad.shobeyri@gmail.com>
2025-09-14 21:44:56 +0200

    Fix issue with logging test result

--- core/src/jsRunner.ts
+++ core/src/jsRunner.ts
@@ -1,4 +1,3 @@
-
 import {LogLevel} from './CommonData';
 import * as mmtHelper from './testHelper';
 
@@ -6,25 +5,27 @@ export async function runJSCode(
     code: string, title: string,
     lg: (level: LogLevel, message: string) => void): Promise<any> {
   lg('info', `Running test ${title}...`);
-  console.log = (...args: any[]) => {
-    lg('info',
-       args.map(a => typeof a === 'string' ? a : JSON.stringify(a)).join(' '));
+
+  const customConsole = {
+    log: (...args: any[]) => lg(
+        'info',
+        args.map(a => typeof a === 'string' ? a : JSON.stringify(a)).join(' ')),
+    error: (...args: any[]) => lg(
+        'error',
+        args.map(a => typeof a === 'string' ? a : JSON.stringify(a)).join(' ')),
+    warn: (...args: any[]) => lg(
+        'warn',
+        args.map(a => typeof a === 'string' ? a : JSON.stringify(a)).join(' ')),
   };
-  console.error = (...args: any[]) => {
-    lg('error',
-       args.map(a => typeof a === 'string' ? a : JSON.stringify(a)).join(' '));
-  };
-  console.warn = (...args: any[]) => {
-    lg('warn',
-       args.map(a => typeof a === 'string' ? a : JSON.stringify(a)).join(' '));
-  };
+
   try {
     const helperDecls =
         Object.keys(mmtHelper)
             .map(name => `const ${name} = mmtHelper["${name}"];`)
             .join('\n');
-    const fn = new Function('mmtHelper', 'lg', `${helperDecls}\n${code}`);
-    await fn(mmtHelper, lg);
+    const fn =
+        new Function('mmtHelper', 'lg', 'console', `${helperDecls}\n${code}`);
+    await fn(mmtHelper, lg, customConsole);
 
     lg('info', 'Done successfully');
   } catch (e: any) {
